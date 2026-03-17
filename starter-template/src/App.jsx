7b1947c4230463bafa336c21ab9bc41b23fa7614
Sanskar Rajput <sanskar.rajput@mindbowser.com>
2026-03-17 18:44:09 +0530

    feat:day 2 assignment

--- starter-template/src/App.jsx
+++ starter-template/src/App.jsx
@@ -3,118 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import UserCard from './UserCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const users = [{ name: "User 1", bio: "This is a bio for user 1", role: "User" }, { name: "User 2", bio: "This is a bio for user 2", role: "User" }, { name: "User 3", bio: "This is a bio for user 3", role: "User" }, { name: "User 4", bio: "This is a bio for user 4", role: "User" }, { name: "User 5", bio: "This is a bio for user 5" }]
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div className='container'>
+      <UserCard user = {{name:"User 1" ,bio:"This is a bio for user 1" ,role:"User"}} />
+      <UserCard user = {{name:"User 2" ,bio:"This is a bio for user 2" ,role:"User"}} />
+      <UserCard user = {{name:"User 3" ,bio:"This is a bio for user 3" ,role:"User"}} />
+      <UserCard user = {{name:"User 4" ,bio:"This is a bio for user 4" ,role:"User"}} />
+      <UserCard user = {{name:"User 5" ,bio:"This is a bio for user 5"}} />
+    </div>
+    // <div className="container">
+    //   {users.map((user) => {
+    //     <UserCard u={user} />
+    //   })}
+    // </div>
   )
 }
 
---
+++ starter-template/src/UserCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UserCard = ({user}) => {
+  const {name,bio,role = "member"} = user;
+  return (
+    <div className="card">
+            <h1>{name}</h1>
+            <p>{bio}</p>
+            <p>{role}</p>
+    </div>
+  )
+}
+
+export default UserCard
