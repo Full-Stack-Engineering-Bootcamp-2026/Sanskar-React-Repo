b3db5274df27bb233eae3fd5657c52d8b058d4ff
Sanskar Rajput <sanskar.rajput@mindbowser.com>
2026-03-18 18:32:05 +0530

    feat:add day 3 assignment

---
+++ starter-template/src/components/LikeButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+
+const LikeButton = () => {
+  const handleClick = ()=>{
+        setCount(prev => prev + 1);
+    }
+    const handleDislikeClick = ()=>{
+        if(count > 0)
+            setCount(prev=>prev - 1)
+    }
+    const [count,setCount] = useState(0);
+    // const [like,setLike] = useState("Like");
+    return (
+    <>
+        <h3>{count} <span>{count > 1?"Likes":"Like"}</span></h3>
+        {/* <button onClick={setCount(prev => prev + 1)}>Like</button> */}
+        <button onClick={handleClick}>Like</button>
+        <button onClick={()=>setCount(0)}>Reset</button> 
+        <button onClick={handleDislikeClick}>Dislike</button>
+    </>
+  )
+}
+
+export default LikeButton
