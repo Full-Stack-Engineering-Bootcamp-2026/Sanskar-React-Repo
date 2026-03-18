49a8fbd99c5952a0dd46ac0ea3088d87fa631194
Sanskar Rajput <sanskar.rajput@mindbowser.com>
2026-03-18 12:32:53 +0530

    feat:add map cards using users array

--- starter-template/src/App.jsx
+++ starter-template/src/App.jsx
@@ -9,18 +9,13 @@ function App() {
 
   const users = [{ name: "User 1", bio: "This is a bio for user 1", role: "User" }, { name: "User 2", bio: "This is a bio for user 2", role: "User" }, { name: "User 3", bio: "This is a bio for user 3", role: "User" }, { name: "User 4", bio: "This is a bio for user 4", role: "User" }, { name: "User 5", bio: "This is a bio for user 5" }]
   return (
-    <div className='container'>
-      <UserCard user = {{name:"User 1" ,bio:"This is a bio for user 1" ,role:"User"}} />
-      <UserCard user = {{name:"User 2" ,bio:"This is a bio for user 2" ,role:"User"}} />
-      <UserCard user = {{name:"User 3" ,bio:"This is a bio for user 3" ,role:"User"}} />
-      <UserCard user = {{name:"User 4" ,bio:"This is a bio for user 4" ,role:"User"}} />
-      <UserCard user = {{name:"User 5" ,bio:"This is a bio for user 5"}} />
+    <div className="container">
+      {users.map((user) => {
+        return (
+        <UserCard user={user} key={user.name} />
+        )
+      })}
     </div>
-    // <div className="container">
-    //   {users.map((user) => {
-    //     <UserCard u={user} />
-    //   })}
-    // </div>
   )
 }
 
--- starter-template/src/UserCard.jsx
+++ starter-template/src/UserCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const UserCard = ({user}) => {
-  const {name,bio,role = "member"} = user;
+
+  const {name, bio, role="member"} = user
+
   return (
     <div className="card">
             <h1>{name}</h1>
-            <p>{bio}</p>
+            <p>{bio}</p>  
             <p>{role}</p>
     </div>
   )
